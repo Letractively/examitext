75b27609bbf4cf69cdb29142c73041a498ba7309
Donald Whyte <datriot@gmail.com>
2011-07-21 15:17:08 +0100

    Added two new name-message format parsers to the list, making a total of three different ways the log is allowed to be structured for the program to work correctly.

--- js/splitter.js
+++ js/splitter.js
@@ -19,7 +19,9 @@ function splitNameAndMessage(text) {
  * match that format. */
 var matches = [
 
-	// If text STARTS with something in square brackets (e.g. "[DJ]Hello.")
+	/* If text contains something in square brackets which has a colon
+	 * straight after it (e.g. "[Name]:Message"), then treat whatever is
+	 * in the square brackets as the name.. */
 	function(text) {	
 		var splittedText = text.split(/\[(.*)\]:/);
 		if (splittedText.length > 1) {
@@ -36,6 +38,44 @@ var matches = [
 		} else {
 			return { "name" : "", "content" : text };
 		}
+	},
+
+	/* If text contains "says:" or "says\n" (\n being a new line), take
+	 * everything BEFORE "says:" as the name, and everything after as
+	 * the message. */
+	function(text) {
+		var index = text.indexOf("says:");
+		// If the substring could not be found, try the same but with
+		// a newline instead of a colon
+		if (index == -1) {
+			index = text.indexOf("says\n");
+		}
+		
+		// If it's been found, get the name
+		var name = "";
+		if (index >= 0) {
+			name = text.slice(0, index);
+			text = text.substring(index + 5); // remove "says:""
+		}
+
+		return { "name" : name, "content" : text };
+	},
+
+	/* If text's first word ends with a colon, assume that the first
+	 * word is a name, and the text after that the colon is the message.
+	 * As soon as this function comes across a space, it aborts and
+	 * stops trying to find the name. */
+	function(text) {
+		for (var i = 0; (i < text.length); ++i) {
+			if (text.charAt(i) == ':') {
+				return { "name" : text.substring(0, i), "content" : text.substring(i + 1) };
+			} else if (text.charAt(i) == ' ') {
+				return { "name" : "", "content" : text };
+			}
+		}
+
+		// If a name hasn't been found now, just return the default
+		return { "name" : "", "content" : text };
 	}
 
 ];
