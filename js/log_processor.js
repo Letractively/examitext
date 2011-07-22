b8676ab1d5dd67edfafb29f13e9a33c7a7cb9bdb
Donald Whyte <datriot@gmail.com>
2011-07-22 12:07:33 +0100

    Done some more work on the website's layout and merged splitter.js into log_processor.js as I felt it was redundant to keep the code separated since they're so closely related.

--- js/log_processor.js
+++ js/log_processor.js
@@ -1,4 +1,4 @@
-/* This file actually TODO */
+/* This file c */
 
 function processMessage(text) {
 	var message = splitNameAndMessage(text);
@@ -14,4 +14,100 @@ function processLog(log) {
 		messages.push(processMessage(messagesAsText[i]));
 	}
 	return messages;
+}
+
+
+/* Attempts to split the given text into a { name : message } pair,
+ * where name is the name of the sender and message is the actual
+ * content of the message. If no name is found, the name field is
+ * just left blank. */
+function splitNameAndMessage(text) {
+	var index = 0;
+	var message = { "name" : "", "content" : text };
+
+	while (index < matches.length && message.name == "") {
+		message = matches[index](text);
+		++index;
+	}
+
+	// Makes sure there's no whitespace in the name
+	message.name = trim(message.name);
+
+	return message;
+}
+
+/* Each of these functions attempts to search for a different name
+ * specifying format, return an empty string if the text doesn't
+ * match that format. There are multiple ways because the chat log
+ * the user provides could be formatted in any way, so as many format
+ * parsers as possible are provided to cover most cases. */
+var matches = [
+
+	/* If text contains something in square brackets which has a colon
+	 * straight after it (e.g. "[Name]:Message"), then treat whatever is
+	 * in the square brackets as the name.. */
+	function(text) {	
+		var splittedText = text.split(/\[(.*)\]:/);
+		if (splittedText.length > 1) {
+			// Ignore the first element since that was BEFORE the name
+			// If it's not empty, it'll just be metadata like time sent and stuff
+			var name = splittedText[1];
+			var content = "";
+
+			for (var i = 2; (i < splittedText.length); ++i) {
+				content += splittedText[i];
+			}
+			
+			return { "name" : name, "content" : content };
+		} else {
+			return { "name" : "", "content" : text };
+		}
+	},
+
+	/* If text contains "says:" or "says\n" (\n being a new line), take
+	 * everything BEFORE "says:" as the name, and everything after as
+	 * the message. */
+	function(text) {
+		var index = text.indexOf("says:");
+		// If the substring could not be found, try the same but with
+		// a newline instead of a colon
+		if (index == -1) {
+			index = text.indexOf("says\n");
+		}
+		
+		// If it's been found, get the name
+		var name = "";
+		if (index >= 0) {
+			name = text.slice(0, index);
+			text = text.substring(index + 5); // remove "says:""
+		}
+
+		return { "name" : name, "content" : text };
+	},
+
+	/* If text's first word ends with a colon, assume that the first
+	 * word is a name, and the text after that the colon is the message.
+	 * As soon as this function comes across a space, it aborts and
+	 * stops trying to find the name. */
+	function(text) {
+		for (var i = 0; (i < text.length); ++i) {
+			if (text.charAt(i) == ':') {
+				return { "name" : text.substring(0, i), "content" : text.substring(i + 1) };
+			} else if (text.charAt(i) == ' ') {
+				return { "name" : "", "content" : text };
+			}
+		}
+
+		// If a name hasn't been found now, just return the default
+		return { "name" : "", "content" : text };
+	}
+
+];
+
+/* Remove multiple, leading or trailing spaces */
+function trim(s) {
+	s = s.replace(/(^\s*)|(\s*$)/gi,"");
+	s = s.replace(/[ ]{2,}/gi," ");
+	s = s.replace(/\n /,"\n");
+	return s;
 }
