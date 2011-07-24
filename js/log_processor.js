49fec0a87fba9ee5fcb74be420485595acd5be11
Donald Whyte <datriot@gmail.com>
2011-07-24 14:21:19 +0100

    Refractored sanitising, stemming and stoplist code so it can support multiple languages.

--- js/log_processor.js
+++ js/log_processor.js
@@ -269,22 +269,6 @@ function toSortedTuples(obj) {
     return tuples;
 }
 
-/* Sanitises an English word by removing all non-alphanumeric characters,
- * whitespace and not returning words less than 3 characters long. */
-function sanitiseWord(word) {
-	// Removes non-alphanumeric characters from the word, EXCEPT for dashes!
-	// NOTE: Use /[^a-zA-Z0-9]+/g to remove dashes too	
-	var sanitisedWord = word.replace(/[^a-zA-Z0-9-]+/g, '');
-	// Makes sure to return an empty string if the word is less than 3
-	// characters long. This tells the stemmer to just completely
-	// ignore the word.
-	if (sanitisedWord.length < 3) {
-		return "";
-	} else {
-		return sanitisedWord;
-	}
-}
-
 /* End of utility functions. */
 
 /* MAIN FUNCTION */
--- js/stemmer.js
+++ js/stemmer.js
@@ -1,186 +1,203 @@
 /* Start of class Stemmer */
-
+  
 function Stemmer(sanitiseFunction, stemFunction) {
-  this.sanitise = sanitiseFunction;
-  this.stemWord = stemFunction;
+    this.sanitise = sanitiseFunction;
+    this.stemWord = stemFunction;
 }
 
 Stemmer.prototype.stemText = function(text) {    
-  var words = text.split(" ");
-  var stemmedText = "";
-  for (var i in words) {
-    // TODO: may have problems with 'this' value on stemWord function
-    var sanitisedWord = this.sanitise(words[i]);
-    // Only stem strings that aren't empty
-    if (sanitisedWord.length > 0) {
-      stemmedText += this.stemWord(sanitisedWord) + " ";
+    var words = text.split(" ");
+    var stemmedText = "";
+    for (var i in words) {
+        var sanitisedWord = this.sanitise(words[i]);
+        // Only stem strings that aren't empty
+        if (sanitisedWord.length > 0) {
+            // NOTE: may cause problems with 'this' parameter in future
+            stemmedText += this.stemWord(sanitisedWord) + " ";
+        }
     }
-  }
-  return stemmedText;
+    return stemmedText;
 }
 
 /* End of class Stemmer */
 
-// Stemmer from: https://github.com/cwolves/stem/blob/master/en.js
-// implemented from algorithm at http://snowball.tartarus.org/algorithms/english/stemmer.html
-var exceptions = {
-  skis    : 'ski'   ,
-  skies   : 'sky'   ,
-  dying   : 'die'   ,
-  lying   : 'lie'   ,
-  tying   : 'tie'   ,
-  idly    : 'idl'   ,
-  gently  : 'gentl' ,
-  ugly    : 'ugli'  ,
-  early   : 'earli' ,
-  only    : 'onli'  ,
-  singly  : 'singl' ,
-  sky     : 'sky'   ,
-  news    : 'news'  ,
-  howe    : 'howe'  ,
-  atlas   : 'atlas' ,
-  cosmos  : 'cosmos',
-  bias    : 'bias'  ,
-  andes   : 'andes'
+// Array which contains every language's stemmer
+var STEMMERS = [
 
-}, exceptions1a = {
-  inning  : 'inning' ,
-  outing  : 'outing' , 
-  canning : 'canning',
-  herring : 'herring',
-  earring : 'earring',
-  proceed : 'proceed',
-  exceed  : 'exceed' ,
-  succeed : 'succeed'
+    // English
+    // Stemmer from: https://github.com/cwolves/stem/blob/master/en.js
+    // implemented from algorithm at http://snowball.tartarus.org/algorithms/english/stemmer.html
+    function (word) {
+        // TODO: find some way of only declaring these ONCE! Not every tme the function is called.
+        var exceptions = {
+          skis    : 'ski'   ,
+          skies   : 'sky'   ,
+          dying   : 'die'   ,
+          lying   : 'lie'   ,
+          tying   : 'tie'   ,
+          idly    : 'idl'   ,
+          gently  : 'gentl' ,
+          ugly    : 'ugli'  ,
+          early   : 'earli' ,
+          only    : 'onli'  ,
+          singly  : 'singl' ,
+          sky     : 'sky'   ,
+          news    : 'news'  ,
+          howe    : 'howe'  ,
+          atlas   : 'atlas' ,
+          cosmos  : 'cosmos',
+          bias    : 'bias'  ,
+          andes   : 'andes'
 
-}, extensions2 = {
-  ization : 'ize' , 
-  fulness : 'ful' , 
-  iveness : 'ive' , 
-  ational : 'ate' , 
-  ousness : 'ous' ,  
-  tional  : 'tion', 
-  biliti  : 'ble' , 
-  lessli  : 'less', 
-  entli   : 'ent' , 
-  ation   : 'ate' , 
-  alism   : 'al'  , 
-  aliti   : 'al'  , 
-  ousli   : 'ous' , 
-  iviti   : 'ive' , 
-  fulli   : 'ful' , 
-  enci    : 'ence', 
-  anci    : 'ance', 
-  abli    : 'able', 
-  izer    : 'ize' , 
-  ator    : 'ate' , 
-  alli    : 'al'  , 
-  bli     : 'ble' , 
-  ogi     : 'og'  , 
-  li      : ''
-};
+        }, exceptions1a = {
+          inning  : 'inning' ,
+          outing  : 'outing' , 
+          canning : 'canning',
+          herring : 'herring',
+          earring : 'earring',
+          proceed : 'proceed',
+          exceed  : 'exceed' ,
+          succeed : 'succeed'
 
-function stemmer(word) {
-  if(word.length < 3){ return word; }
-  if(exceptions[word]){ return exceptions[word]; }
+        }, extensions2 = {
+          ization : 'ize' , 
+          fulness : 'ful' , 
+          iveness : 'ive' , 
+          ational : 'ate' , 
+          ousness : 'ous' ,  
+          tional  : 'tion', 
+          biliti  : 'ble' , 
+          lessli  : 'less', 
+          entli   : 'ent' , 
+          ation   : 'ate' , 
+          alism   : 'al'  , 
+          aliti   : 'al'  , 
+          ousli   : 'ous' , 
+          iviti   : 'ive' , 
+          fulli   : 'ful' , 
+          enci    : 'ence', 
+          anci    : 'ance', 
+          abli    : 'able', 
+          izer    : 'ize' , 
+          ator    : 'ate' , 
+          alli    : 'al'  , 
+          bli     : 'ble' , 
+          ogi     : 'og'  , 
+          li      : ''
+        };
 
-  var eRx = ['', ''],
-     word = word.toLowerCase().replace(/^'/, '').replace(/[^a-z']/g, '').replace(/^y|([aeiouy])y/g, '$1Y'),
-            R1, res;
+        if(word.length < 3){ return word; }
+        if(exceptions[word]){ return exceptions[word]; }
 
-  if(res = /^(gener|commun|arsen)/.exec(word)){
-    R1 = res[0].length;
-  }else{
-    R1 = ((/[aeiouy][^aeiouy]/.exec(' '+word) || eRx).index || 1000) + 1;
-  }
+        var eRx = ['', ''],
+           word = word.toLowerCase().replace(/^'/, '').replace(/[^a-z']/g, '').replace(/^y|([aeiouy])y/g, '$1Y'),
+                  R1, res;
 
-  var R2 = (((/[aeiouy][^aeiouy]/.exec(' '+word.substr(R1)) || eRx).index || 1000)) + R1 + 1;
+        if(res = /^(gener|commun|arsen)/.exec(word)){
+          R1 = res[0].length;
+        }else{
+          R1 = ((/[aeiouy][^aeiouy]/.exec(' '+word) || eRx).index || 1000) + 1;
+        }
+
+        var R2 = (((/[aeiouy][^aeiouy]/.exec(' '+word.substr(R1)) || eRx).index || 1000)) + R1 + 1;
 
 
-  // step 0
-  word = word.replace(/('s'?|')$/, '');
+        // step 0
+        word = word.replace(/('s'?|')$/, '');
 
 
-  // step 1a
-  rx = /(?:(ss)es|(..i)(?:ed|es)|(us)|(ss)|(.ie)(?:d|s))$/;
-  if(rx.test(word)){
-    word = word.replace(rx, '$1$2$3$4$5');
-  }else{
-    word = word.replace(/([aeiouy].+)s$/, '$1');
-  }
+        // step 1a
+        rx = /(?:(ss)es|(..i)(?:ed|es)|(us)|(ss)|(.ie)(?:d|s))$/;
+        if(rx.test(word)){
+          word = word.replace(rx, '$1$2$3$4$5');
+        }else{
+          word = word.replace(/([aeiouy].+)s$/, '$1');
+        }
 
-  if(exceptions1a[word]){ return exceptions1a[word]; }
+        if(exceptions1a[word]){ return exceptions1a[word]; }
 
-  // step 1b
-  var s1 = (/(eedly|eed)$/.exec(word) || eRx)[1],
-      s2 = (/(?:[aeiouy].*)(ingly|edly|ing|ed)$/.exec(word) || eRx)[1];
+        // step 1b
+        var s1 = (/(eedly|eed)$/.exec(word) || eRx)[1],
+            s2 = (/(?:[aeiouy].*)(ingly|edly|ing|ed)$/.exec(word) || eRx)[1];
 
-  if(s1.length > s2.length){
-    if(word.indexOf(s1, R1)>=0){
-      word = word.substr(0, word.length - s1.length) + 'ee';
+        if(s1.length > s2.length){
+          if(word.indexOf(s1, R1)>=0){
+            word = word.substr(0, word.length - s1.length) + 'ee';
+          }
+        }else if(s2.length > s1.length){
+          word = word.substr(0, word.length - s2.length);
+          if(/(at|bl|iz)$/.test(word)){
+            word += 'e';
+          }else if(/(bb|dd|ff|gg|mm|nn|pp|rr|tt)$/.test(word)){
+            word = word.substr(0, word.length - 1);
+          }else if(!word.substr(R1) && /([^aeiouy][aeiouy][^aeiouywxY]|^[aeiouy][^aeiouy]|^[aeiouy])$/.test(word)){
+            word += 'e';
+          }
+        }
+
+
+        // step 1c
+        word = word.replace(/(.[^aeiouy])[yY]$/, '$1i');
+
+
+        // step 2
+        var sfx = /(ization|fulness|iveness|ational|ousness|tional|biliti|lessli|entli|ation|alism|aliti|ousli|iviti|fulli|enci|anci|abli|izer|ator|alli|bli|l(ogi)|[cdeghkmnrt](li))$/.exec(word);
+        if(sfx){
+          sfx  = sfx[3] || sfx[2] || sfx[1];
+          if(word.indexOf(sfx, R1) >= 0){
+            word = word.substr(0, word.length - sfx.length) + extensions2[sfx];
+          }
+        }
+
+
+        // step 3
+        var sfx = (/(ational|tional|alize|icate|iciti|ative|ical|ness|ful)$/.exec(word) || eRx)[1];
+        if(sfx && (word.indexOf(sfx, R1) >= 0)){
+          word = word.substr(0, word.length - sfx.length) + {
+            ational : 'ate',
+            tional  : 'tion',
+            alize   : 'al',
+            icate   : 'ic',
+            iciti   : 'ic',
+            ative   : ((word.indexOf('ative', R2) >= 0) ? '' : 'ative'),
+            ical    : 'ic',
+            ness    : '',
+            ful     : ''
+          }[sfx];
+        }
+
+
+        // step 4
+        var sfx = /(ement|ance|ence|able|ible|ment|ant|ent|ism|ate|iti|ous|ive|ize|[st](ion)|al|er|ic)$/.exec(word);
+        if(sfx){
+          sfx = sfx[2] || sfx[1];
+          if(word.indexOf(sfx, R2) >= 0){
+            word = word.substr(0, word.length - sfx.length);
+          }
+        }
+
+
+        // step 5
+        if(word.substr(-1) == 'e'){
+          if(word.substr(R2) || (word.substr(R1) && !(/([^aeiouy][aeiouy][^aeiouywxY]|^[aeiouy][^aeiouy])e$/.test(word)))){
+            word = word.substr(0, word.length - 1);
+          }
+
+        }else if((word.substr(-2) == 'll') && (word.indexOf('l', R2) >= 0)){
+          word = word.substr(0, word.length - 1);
+        }
+
+        return word.toLowerCase();
+    },
+
+    // French
+    function (word) {
+      // TODO
+    },
+
+    // German
+    function (word) {
+      // TODO
     }
-  }else if(s2.length > s1.length){
-    word = word.substr(0, word.length - s2.length);
-    if(/(at|bl|iz)$/.test(word)){
-      word += 'e';
-    }else if(/(bb|dd|ff|gg|mm|nn|pp|rr|tt)$/.test(word)){
-      word = word.substr(0, word.length - 1);
-    }else if(!word.substr(R1) && /([^aeiouy][aeiouy][^aeiouywxY]|^[aeiouy][^aeiouy]|^[aeiouy])$/.test(word)){
-      word += 'e';
-    }
-  }
 
-
-  // step 1c
-  word = word.replace(/(.[^aeiouy])[yY]$/, '$1i');
-
-
-  // step 2
-  var sfx = /(ization|fulness|iveness|ational|ousness|tional|biliti|lessli|entli|ation|alism|aliti|ousli|iviti|fulli|enci|anci|abli|izer|ator|alli|bli|l(ogi)|[cdeghkmnrt](li))$/.exec(word);
-  if(sfx){
-    sfx  = sfx[3] || sfx[2] || sfx[1];
-    if(word.indexOf(sfx, R1) >= 0){
-      word = word.substr(0, word.length - sfx.length) + extensions2[sfx];
-    }
-  }
-
-
-  // step 3
-  var sfx = (/(ational|tional|alize|icate|iciti|ative|ical|ness|ful)$/.exec(word) || eRx)[1];
-  if(sfx && (word.indexOf(sfx, R1) >= 0)){
-    word = word.substr(0, word.length - sfx.length) + {
-      ational : 'ate',
-      tional  : 'tion',
-      alize   : 'al',
-      icate   : 'ic',
-      iciti   : 'ic',
-      ative   : ((word.indexOf('ative', R2) >= 0) ? '' : 'ative'),
-      ical    : 'ic',
-      ness    : '',
-      ful     : ''
-    }[sfx];
-  }
-
-
-  // step 4
-  var sfx = /(ement|ance|ence|able|ible|ment|ant|ent|ism|ate|iti|ous|ive|ize|[st](ion)|al|er|ic)$/.exec(word);
-  if(sfx){
-    sfx = sfx[2] || sfx[1];
-    if(word.indexOf(sfx, R2) >= 0){
-      word = word.substr(0, word.length - sfx.length);
-    }
-  }
-
-
-  // step 5
-  if(word.substr(-1) == 'e'){
-    if(word.substr(R2) || (word.substr(R1) && !(/([^aeiouy][aeiouy][^aeiouywxY]|^[aeiouy][^aeiouy])e$/.test(word)))){
-      word = word.substr(0, word.length - 1);
-    }
-
-  }else if((word.substr(-2) == 'll') && (word.indexOf('l', R2) >= 0)){
-    word = word.substr(0, word.length - 1);
-  }
-
-  return word.toLowerCase();
-};
+];
